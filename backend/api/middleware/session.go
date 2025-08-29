65b50b7a0d6a16b59158b64188228fea8297deb2
wingjson <wingjson@outlook.com>
2025-08-29 10:33:57 +0800

    feat:replace qiankun to wujie

--- backend/api/middleware/session.go
+++ backend/api/middleware/session.go
@@ -35,7 +35,7 @@ import (
 var noNeedSessionCheckPath = map[string]bool{
 	"/api/passport/web/email/login/":       true,
 	"/api/passport/web/email/register/v2/": true,
-	"/api/sso/check-login": true,
+	"/api/sso/check-login":                 true,
 }
 
 func SessionAuthMW() app.HandlerFunc {
